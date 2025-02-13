279e76bbeea780159eec509124e83c6bcd802628
C0D3Dr4G0N <brennanj18@southernct.edu>
2025-02-12 20:13:30 -0500

    Converted code in main.py to a function

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
 
-# Fix 1: Initialize variable `total`
-total = 0
+def AddNum():
+    # Fix 1: Initialize variable `total`
+    total = 0
 
-for i in range(5):
-    # Fix 2: Convert input into an integer
-    number = int(input("Enter a number: "))
-    total += number
+    for i in range(5):
+        # Fix 2: Convert input into an integer
+        number = int(input("Enter a number: "))
+        total += number
 
-# Fix 3: Add comma to separate arguments to print function
-print("The running total is: ", total)
+    # Fix 3: Add comma to separate arguments to print function
+
+    return total
+
+def main():
+    result = AddNum()
+    print("The running total is: ", result)
+
+if __name__ == '__main__':
+    main()
