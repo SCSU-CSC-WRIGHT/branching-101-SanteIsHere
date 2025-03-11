80bc6d24594819bb363b34795e5563b7c70ebe1a
Asante Frye <86087030+SanteIsHere@users.noreply.github.com>
2025-03-10 22:16:02 -0400

    Update main.py
    Restore `main.py` to default for lab assignment

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
 
-def AddNum():
-    # Fix 1: Initialize variable `total`
-    total = 0
+for i in range(5):
+    number = input("Enter a number: ")
+    total += number
 
-    for i in range(5):
-        # Fix 2: Convert input into an integer
-        number = int(input("Enter a number: "))
-        total += number
-
-    # Fix 3: Add comma to separate arguments to print function
-
-    return total
-
-def main():
-    result = AddNum()
-    print("The running total is: ", result)
-
-if __name__ == '__main__':
-    main()
+print("The running total is: "  total)
